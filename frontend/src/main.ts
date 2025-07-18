6139b73df21693fe9842575edb7109e9bc171070
Pablo Azevedo <pabloslayer@gmail.com>
2025-07-18 18:28:47 -0300

    feat(frontend): update main file

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { lazyLoadDirective } from './directives/lazyLoad'
 
 const app = createApp(App)
 
 app.use(router)
+app.directive('lazy-load', lazyLoadDirective);
 
 app.mount('#app')
