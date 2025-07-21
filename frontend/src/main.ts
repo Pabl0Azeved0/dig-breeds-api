5caa63ab280c408f39ae72d367c835e3db62311a
Pablo Azevedo <pabloslayer@gmail.com>
2025-07-20 22:21:05 -0300

    hotfix(frontend): Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import { lazyLoadDirective } from './directives/lazyLoad'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
-app.use(router)
-app.directive('lazy-load', lazyLoadDirective);
-
+app.use(vuetify)
+app.directive('lazy-load', lazyLoadDirective)
 app.mount('#app')
